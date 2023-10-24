81354efc26b2ea631e6ec2258cf0a337d3f9a4d9
AndyBalago <andres.balagon@gmail.com>
2023-10-24 09:29:58 +0200

    Creation menu responsive avec ses actions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import LogoST from '../Img/LogoST.png'
-import { NavLink } from 'react-router-dom'
+import { NavLink} from 'react-router-dom'
 
 import { useSelector } from 'react-redux'
 import { AiOutlineMenu } from 'react-icons/ai'
@@ -8,12 +8,16 @@ import { AiOutlineMenu } from 'react-icons/ai'
 
 const NavBar = () => {
   const products = useSelector((state) => state.productReducer);
+  
 
   const allCategories = [...new Set(products.map(product => product.categories))];
 
   const [showNavBar, setShowNavBar] = useState(false);
   const handleShowNavBar = () => {
     setShowNavBar(!showNavBar)
+  };
+  const closeNavBar = () => {
+    setShowNavBar(false);
   };
 
 
@@ -27,11 +31,11 @@ const NavBar = () => {
       <div className='menuResponsive'>
         <AiOutlineMenu className='menu-icon' onClick={handleShowNavBar}/>
       </div>
-      <div className={`categoriesBtns  ${showNavBar && 'active'}`} >
+      <div className={`categoriesBtns  ${showNavBar && 'active'}`} onClick={closeNavBar} >
+      <div className='contBtnsCate'>
         {allCategories.map((category, index) => {
           return (
-            <div key={index} className='cate'>
-              <NavLink to={"/filteredProducts/" + category}>
+              <NavLink key={index} to={"/filteredProducts/" + category}>
                 <button
                   className='btnCategorie'
                   type='button'
@@ -39,12 +43,12 @@ const NavBar = () => {
                 >{category}
                 </button>
               </NavLink>
-            </div>
           )
         })}
       </div>
       <div className='contCreer'>
         <NavLink to='/create' ><button className='btnCreer'>Creer produit</button></NavLink>
+      </div>
       </div>
     </div>
   )
